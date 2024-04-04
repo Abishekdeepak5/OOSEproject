eeb88f868d0c151e9533d4057e9d059f656ca6df
unknown <Abishek Aranganathan>
2024-04-04 11:11:39 +0530

    Hello

---
+++ train/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Train(models.Model):
+    train_no=models.BigAutoField(primary_key=True)
+    train_name=models.CharField(max_length=30)
+    capacity=models.IntegerField()
+
+class Station(models.Model):
+    station_id=models.BigAutoField(primary_key=True)
+    station_name=models.CharField(max_length=30)
+    station_location=models.CharField(max_length=100)
+
+class Route(models.Model):
+    route_no=models.BigAutoField(primary_key=True)
+    train_no=models.ForeignKey(Train)
+    from_location=models.CharField(max_length=30)
+    to_location=models.CharField(max_length=30)
+    arrival_time=models.TimeField()
+    departure_time=models.TimeField()
+    station_id=models.ForeignKey(Station)
+
